7dd8bb3d16709767f35cb543656263b906e84edb
Polina <vecherdobrii007@gmail.com>
2022-06-22 00:46:39 +0300

    ctr done

---
+++ DES/CTR.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Cryptography.Extensions;
+
+namespace DES
+{
+    public class CTR
+    {
+        private ISymmetricalAlgorithm _encoder;
+        
+        public CTR(ISymmetricalAlgorithm encryptor)
+        {
+            _encoder = encryptor;
+        }
+        
+        public byte[] EncryptBlock(byte[] message, byte[][] roundKeys, object[] initializationVector)
+        {
+            var result = new byte[message.Length];
+            var prevBlock = new byte[_encoder.BlockSize];
+            
+            Array.Copy((byte[])initializationVector[0], prevBlock, prevBlock.Length);
+            for (var i = 0; i < result.Length / _encoder.BlockSize; i++)
+            {
+                var currentBlock = message.Skip(i * _encoder.BlockSize).Take(_encoder.BlockSize).ToArray();
+                
+                prevBlock = _encoder.Encrypt(prevBlock, roundKeys);
+                currentBlock = prevBlock.Xor(currentBlock);
+                Array.Copy(currentBlock, 0, result, i * _encoder.BlockSize, _encoder.BlockSize);
+            }
+
+            return result;
+        }
+        
+        public byte[] DecryptBlock(byte[] message, byte[][] roundKeys, object[] initializationVector)
+        {
+            var result = EncryptBlock(message, roundKeys, initializationVector);
+            Array.Resize(ref result, message.Length - result[^1]);
+            
+            return result;
+        }
+    }
+}
