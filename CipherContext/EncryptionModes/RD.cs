8836e243afb7313385ba48eac1f267e263538832
Polina <vecherdobrii007@gmail.com>
2022-06-26 01:46:21 +0300

    rd + parallel processing done

--- CipherContext/EncryptionModes/RD.cs
+++ CipherContext/EncryptionModes/RD.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using SymmetricalAlgorithm;
 using static Cryptography.Extensions.ByteArrayExtensions;
 
@@ -16,43 +18,51 @@ namespace CipherContext.EncryptionModes
         
         public byte[] EncryptBlock(byte[] message, byte[][] roundKeys, object[] initializationVector)
         {
-            var result = new byte[message.Length + _encoder.BlockSize];
             var initial = new byte[_encoder.BlockSize];
+            Array.Copy((byte[])initializationVector[0], initial, _encoder.BlockSize);
             
-            Array.Copy((byte[])initializationVector[0], initial, initial.Length);
-            Array.Copy(_encoder.Encrypt(initial, roundKeys), 0, result, 0, _encoder.BlockSize);
+            var initialBlock = _encoder.Encrypt(initial, roundKeys);
             //для дельты всегда берем последние 8 байт вектора инициализации
             var delta = initial.Skip(initial.Length - 8).ToArray();
             //делаем дельту нечетной
             delta[0] |= 0x01;
+            
+            var tasks = new List<Task<byte[]>>();
             for (var i = 0; i < message.Length / _encoder.BlockSize; i++)
             {
-                var currentBlock = message.Skip(i * _encoder.BlockSize).Take(_encoder.BlockSize).ToArray();
-                currentBlock = initial.Xor(currentBlock);
-                currentBlock = _encoder.Encrypt(currentBlock, roundKeys);
-                
-                Array.Copy(currentBlock, 0, result, (i + 1) * _encoder.BlockSize, _encoder.BlockSize);
+                var i1 = i;
                 initial = ByteArrayAdditionByModulo2PowN(initial, delta);
+                var initial1 = initial;
+                tasks.Add(Task.Run(() =>
+                {
+                    var currentBlock = message.Skip(i1 * _encoder.BlockSize).Take(_encoder.BlockSize).ToArray();
+                    return _encoder.Encrypt(initial1.Xor(currentBlock), roundKeys);
+                }));
             }
-
-            return result;
+            var result =  Task.WhenAll(tasks);
+            
+            return initialBlock.Concat(result.Result.SelectMany(block => block).ToArray()).ToArray();
         }
         
         public byte[] DecryptBlock(byte[] message, byte[][] roundKeys)
         {
-            var result = new byte[message.Length - _encoder.BlockSize];
             var initial =  _encoder.Decrypt(message.Take(_encoder.BlockSize).ToArray(), roundKeys);
-            
             var delta = initial.Skip(initial.Length - 8).ToArray();
+
+            var tasks = new List<Task<byte[]>>();
             for (var i = 1; i < message.Length / _encoder.BlockSize; i++)
             {
-                var currentBlock = message.Skip(i * _encoder.BlockSize).Take(_encoder.BlockSize).ToArray();
-                currentBlock = _encoder.Decrypt(currentBlock, roundKeys);
-                currentBlock = initial.Xor(currentBlock);
-                
-                Array.Copy(currentBlock, 0, result, (i - 1) * _encoder.BlockSize, _encoder.BlockSize);
-                initial =  ByteArrayAdditionByModulo2PowN(initial, delta);
+                var i1 = i;
+                initial = ByteArrayAdditionByModulo2PowN(initial, delta);
+                var initial1 = initial;
+                tasks.Add(Task.Run(() =>
+                {
+                    var currentBlock =message.Skip(i1 * _encoder.BlockSize).Take(_encoder.BlockSize).ToArray();
+                    return initial1.Xor(_encoder.Decrypt(currentBlock, roundKeys));
+                }));
             }
+            var decrypted =  Task.WhenAll(tasks);
+            var result = decrypted.Result.SelectMany(block => block).ToArray();
             Array.Resize(ref result, result.Length - result[^1]);
             
             return result;
