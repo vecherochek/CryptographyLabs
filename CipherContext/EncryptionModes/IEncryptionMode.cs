80b8631c6b7cef18cfd1b7d1e7fb758a1a3a8edf
Polina <vecherdobrii007@gmail.com>
2022-08-30 23:59:31 +0300

    created interface for encryption modes

---
+++ CipherContext/EncryptionModes/IEncryptionMode.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CipherContext.EncryptionModes
+{
+    public interface IEncryptionMode
+    {
+        public Task<byte[]> EncryptBlockAsync(byte[] message, byte[][] roundKeys, params object[] values);
+        public Task<byte[]> DecryptBlockAsync(byte[] message, byte[][] roundKeys, params object[] values);
+    }
+}
