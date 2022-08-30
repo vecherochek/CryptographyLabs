9a107524e4c6a3f3112a48b2deaecdafc00e4bf3
Polina <vecherdobrii007@gmail.com>
2022-08-31 00:03:49 +0300

    some minor changes

--- Cryptography.Extensions/ByteArrayExtensions.cs
+++ Cryptography.Extensions/ByteArrayExtensions.cs
@@ -14,71 +14,73 @@ namespace Cryptography.Extensions
             {
                 changed[i] = bitsblock[permutationTable[i] - 1];
             }
-            
+
             return BitArrayToByteArray(changed);
         }
+
         public static byte[] BitArrayToByteArray(BitArray bits)
         {
             var bytes = new byte[(bits.Length - 1) / 8 + 1];
             bits.CopyTo(bytes, 0);
             return bytes;
         }
+
         public static byte[] PermutationSBlock(byte[] block, byte[,,] permutationTable)
         {
             var number = BitConverter.ToUInt32(block, 0);
             ulong result = 0;
             for (var i = 0; i < 8; i++)
             {
-                var B = (number >> (i * 6)) & ((uint)1 << 6) - 1;
+                var B = (number >> (i * 6)) & ((uint) 1 << 6) - 1;
                 var a = ((B >> 5) << 1) | (B & 1);
                 var b = (B >> 1) & 0b1111;
 
                 B = permutationTable[i, a, b];
-                result|= B << i * 4;
+                result |= B << i * 4;
             }
-            
+
             return BitConverter.GetBytes(result);
         }
-        
+
         public static byte[] Xor(this byte[] left, byte[] right)
         {
             if (left is null)
             {
                 throw new ArgumentNullException(nameof(left));
             }
-            
+
             if (right is null)
             {
                 throw new ArgumentNullException(nameof(right));
             }
-            
+
             if (left.Length != right.Length)
             {
                 throw new ArgumentException("Arrays lengths must be equal.");
             }
-            
-            var result = new byte[left.Length];
+
             for (var i = 0; i < left.Length; i++)
             {
-                result[i] = (byte) (left[i] ^ right[i]);
+                left[i] = (byte) (left[i] ^ right[i]);
             }
 
-            return result;
+            return left;
         }
-        
+
         public static byte[] PaddingPKCs7(byte[] block, int blockSize)
         {
             var addition = (byte) (blockSize - block.Length % blockSize);
             var paddedBlock = new byte[block.Length + addition];
             Array.Copy(block, paddedBlock, block.Length);
-            Array.Fill(paddedBlock, addition, block.Length, addition); 
-            
+            Array.Fill(paddedBlock, addition, block.Length, addition);
+
             return paddedBlock;
         }
+
         public static byte[] ByteArrayAdditionByModulo2PowN(byte[] left, byte[] right)
         {
             BitArray num1, num2;
-            
+
             if (left.Length > right.Length)
             {
                 num1 = new BitArray(left);
